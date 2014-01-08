228c8a933915081bf555bf6381096f28d0e5a422
darknrgy <darknrgy@>
2014-01-07 19:06:50 -0800

    trip grid first commit

---
+++ Assets/src/spacecraft.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spacecraft : MonoBehaviour {
+
+	public float BufferZone;
+	public float WorldSize;
+	public GameObject OrientationReference;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+
+		float joyX = Input.GetAxis ("Joy0X") * 2;
+		float joyY = Input.GetAxis ("Joy0Y") * 2;
+		float joyZ = Input.GetAxis ("Joy0Z") * 2;
+
+		if (Input.GetButton("Accelerate")){
+			rigidbody.AddRelativeForce(new Vector3(0, 0, 0.50f));
+		}
+
+		if (Input.GetButton("Brake")){
+			//rigidbody.AddRelativeForce(new Vector3(0, 0, -0.50f));
+			rigidbody.velocity = rigidbody.velocity * 0.99f;
+			Debug.Log("fuck you?");
+		}
+
+		if (Input.GetButton("ResetOculus")){
+			OVRDevice.ResetOrientation(0);
+		}
+
+		
+		if (Input.GetButton("Escape")){
+			Application.Quit();
+		}
+
+		rigidbody.AddRelativeTorque(new Vector3(0, 0, -joyX));
+		rigidbody.AddRelativeTorque(new Vector3(-joyY, 0, 0));
+		rigidbody.AddRelativeTorque(new Vector3(0, joyZ, 0));
+
+		if (Input.GetKey(KeyCode.Space)){
+			rigidbody.AddRelativeForce(new Vector3(0, 0, 0.50f));
+		}
+
+		if (Input.GetKey(KeyCode.LeftAlt)){
+			OVRDevice.ResetOrientation(0);
+		}
+
+
+
+		float min = 0 - BufferZone;
+		float max = WorldSize + BufferZone;
+		Vector3 position = rigidbody.transform.position;
+
+		if (position.x > max) position.x = min;
+		if (position.x < min) position.x = max;
+
+		if (position.y > max) position.y = min;
+		if (position.y < min) position.y = max;
+
+		if (position.z > max) position.z = min;
+		if (position.z < min) position.z = max;
+
+		rigidbody.position = position;
+
+
+		Vector3 velocity = rigidbody.velocity;
+		Vector3 orientationVelocity = OrientationReference.transform.position - transform.position;
+		orientationVelocity = orientationVelocity.normalized * velocity.magnitude;
+
+		velocity = velocity * 0.99f + orientationVelocity * 0.01f;
+		rigidbody.velocity = velocity;
+
+
+
+
+
+
+
+
+		
+	}
+}
